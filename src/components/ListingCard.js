b58b8ae0b658aee4b1dca3263858b32af150aa87
NickeaBennett <nickeabennett@gmail.com>
2022-06-20 19:13:47 -0400

    Assignment

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,24 +1,20 @@
 import React from "react";
+import ListingCard from "./ListingCard";
 
-function ListingCard() {
+function ListingsContainer({ listings, onRemoveListing }) {
   return (
-    <li className="card">
-      <div className="image">
-        <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
-      </div>
-      <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
-        ) : (
-          <button className="emoji-button favorite">☆</button>
-        )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
-      </div>
-    </li>
+    <main>
+      <ul className="cards">
+        {listings.map((listing) => (
+          <ListingCard
+            key={listing.id}
+            listing={listing}
+            onRemoveListing={onRemoveListing}
+          />
+        ))}
+      </ul>
+    </main>
   );
 }
 
-export default ListingCard;
+export default ListingsContainer;
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,35 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import ListingsContainer from "./ListingsContainer";
 
-function ListingsContainer() {
+function App() {
+  const [listings, setListings] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((r) => r.json())
+      .then(setListings);
+  }, []);
+
+  function handleRemoveListing(id) {
+    const newListings = listings.filter((listing) => listing.id !== id);
+    setListings(newListings);
+  }
+
+  const displayedListings = listings.filter((listing) =>
+    listing.description.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
-    </main>
+    <div className="app">
+      <Header onSearch={setSearch} />
+      <ListingsContainer
+        listings={displayedListings}
+        onRemoveListing={handleRemoveListing}
+      />
+    </div>
   );
 }
 
-export default ListingsContainer;
+export default App;
